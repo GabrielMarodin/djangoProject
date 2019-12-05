d4acf086d366a280e0ab88ae6693e5efafa7157d
GabrielMarodin <gabrielmarodin@hotmail.com>
2019-12-05 13:28:00 -0300

    ta

--- lfg/user/models.py
+++ lfg/user/models.py
@@ -3,9 +3,9 @@ from datetime import datetime
 
 class User(models.Model):
     username = models.CharField(max_length=200)
-    id = models.IntegerField()
+    id = models.IntegerField(primary_key='true')
     email = models.EmailField()
     password = models.CharField
     data_user = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
-        return self.nome
+        return self.username
